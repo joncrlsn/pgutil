86661c5849e8536cf2921877a180050df212c13e
Jon Carlson <jon@code42.com>
2014-07-06 11:52:40 -0500

    password handling changes

--- pgutil.go
+++ pgutil.go
@@ -6,18 +6,21 @@ import "os"
 import "flag"
 import "fmt"
 import "strconv"
+import "strings"
 import "github.com/joncrlsn/misc"
+import "github.com/joncrlsn/fileutil"
 
 var p = fmt.Println
+var pgPassFile string = ".pgpass"
 
 // Database connection info
 type DbInfo struct {
-    DbName string
-    DbHost string 
-    DbPort int32
-    DbUser string
-    DbPass string
-    DbOptions string
+	DbName    string
+	DbHost    string
+	DbPort    int32
+	DbUser    string
+	DbPass    string
+	DbOptions string
 }
 
 /*
@@ -25,62 +28,103 @@ type DbInfo struct {
  * This calls flag.Parse(), so define any other program flags before calling this.
  */
 func (dbInfo *DbInfo) Populate() {
-    userDefault    := misc.CoalesceStrings(os.Getenv("DBUSER"), "c42ro")
-    hostDefault    := misc.CoalesceStrings(os.Getenv("DBHOST"), "localhost")
-    portDefaultStr := misc.CoalesceStrings(os.Getenv("DBPORT"), "5432")
+	userDefault := misc.CoalesceStrings(os.Getenv("DBUSER"), "c42ro")
+	hostDefault := misc.CoalesceStrings(os.Getenv("DBHOST"), "localhost")
+	portDefaultStr := misc.CoalesceStrings(os.Getenv("DBPORT"), "5432")
+	passDefault := os.Getenv("PGPASS")
 
-    // port is a little different because it's an int
-    portDefault, _ := strconv.Atoi(portDefaultStr)
-    fmt.Println("portDefault", portDefault)
+	// port is a little different because it's an int
+	portDefault, _ := strconv.Atoi(portDefaultStr)
+	fmt.Println("portDefault", portDefault)
 
-    var dbUser = flag.String("user", userDefault,  "db user")
-    var dbPass = flag.String("pw",   "",           "db password")
-    var dbHost = flag.String("host", hostDefault,  "db host")
-    var dbPort = flag.Int("port",    portDefault,  "db port")
-    var dbName = flag.String("db",   "",           "db name")
+	var dbUser = flag.String("U", userDefault, "db user")
+	var dbPass = flag.String("pw", "", "db password")
+	var dbHost = flag.String("h", hostDefault, "db host")
+	var dbPort = flag.Int("p", portDefault, "db port")
+	var dbName = flag.String("d", "", "db name")
 
-    // This will parse all the flags defined for the program.  Not sure how to get around this.
-    flag.Parse()
+	// This will parse all the flags defined for the program.  Not sure how to get around this.
+	flag.Parse()
 
-    if len(*dbUser) > 0 {
-        dbInfo.DbUser = *dbUser
-    }
-    if len(*dbPass) > 0 {
-        dbInfo.DbPass = *dbPass
-    }
-    if len(*dbHost) > 0 {
-        dbInfo.DbHost = *dbHost
-    }
-    if *dbPort > 0 {
-        dbInfo.DbPort = int32(*dbPort)
-    }
-    if len(*dbName) > 0 {
-        dbInfo.DbName = *dbName
-    }
+	if len(*dbUser) > 0 {
+		dbInfo.DbUser = *dbUser
+	}
+	if len(*dbPass) > 0 {
+		dbInfo.DbPass = *dbPass
+	}
+	// the password is a little different because it can also be found in ~/.pgpass
+	if len(dbInfo.DbPass) == 0 {
+		if len(passDefault) > 1 {
+			dbInfo.DbPass = passDefault
+		} else {
+			dbInfo.DbPass = PgPassword(dbInfo.DbUser)
+            if len(dbInfo.DbPass) == 0 {
+                dbInfo.DbPass = misc.Prompt("Enter password: ")
+            }
+		}
+	}
+	if len(*dbHost) > 0 {
+		dbInfo.DbHost = *dbHost
+	}
+	if *dbPort > 0 {
+		dbInfo.DbPort = int32(*dbPort)
+	}
+	if len(*dbName) > 0 {
+		dbInfo.DbName = *dbName
+	}
 }
 
 func (dbInfo *DbInfo) ConnectionString() string {
-    connString := "user=" + dbInfo.DbUser + " host=" + dbInfo.DbHost + " dbname=" + dbInfo.DbName + " password=" + dbInfo.DbPass
-    if len(dbInfo.DbOptions) > 0 {
-        connString += " " + dbInfo.DbOptions
-    }
-    return connString 
+	connString := "user=" + dbInfo.DbUser + " host=" + dbInfo.DbHost + " dbname=" + dbInfo.DbName + " password=" + dbInfo.DbPass
+	if len(dbInfo.DbOptions) > 0 {
+		connString += " " + dbInfo.DbOptions
+	}
+	return connString
 }
 
 /*
  * Opens a postgreSQL database connection using the DbInfo instance
  */
 func (dbInfo *DbInfo) Open() (*sql.DB, error) {
-    conn := dbInfo.ConnectionString()
+	conn := dbInfo.ConnectionString()
 	db, err := sql.Open("postgres", conn)
 	return db, err
 }
 
-/* 
+/*
  * Provides a model for adding to your own database executable
  */
 func DbUsage() {
-	fmt.Fprintf(os.Stderr, "usage: %s [-host <string>] [-port <int>] [-db <string>] [-user <string>] [-password <string>] \n", os.Args[0])
+	fmt.Fprintf(os.Stderr, "usage: %s [-h <string>] [-p <int>] [-d <string>] [-U <string>] [-pw <string>] \n", os.Args[0])
 	flag.PrintDefaults()
 	os.Exit(2)
+}
+
+/*
+ * Parses the ~/.pgpass file and gets the password for the given user.  The current implementation
+ * ignores the location field.
+ */
+func PgPassword(user string) string {
+	pgPassPath := os.Getenv("HOME") + "/" + pgPassFile
+	exists, err := fileutil.Exists(pgPassPath)
+	if err != nil {
+		panic(err)
+	}
+	if !exists {
+		return ""
+	}
+
+	lines, err := fileutil.ReadLinesArray(pgPassPath)
+	if err != nil {
+		panic(err)
+	}
+	for _, line := range lines {
+		if strings.Contains(line, ":"+user+":") {
+			fields := strings.Split(line, ":")
+			password := fields[4]
+            fmt.Println("Used password from ~/.pgpass")
+			return password
+		}
+	}
+	return ""
 }
---
+++ pgutil_test.go
@@ -0,0 +1,25 @@
+package pgutil
+
+import "os"
+import "strings"
+import "testing"
+import "github.com/joncrlsn/fileutil"
+
+func init() {
+	// this override the value in pgutil.go
+	pgPassFile = ".pgpass.testing"
+	testFileName := os.Getenv("HOME") + "/" + pgPassFile
+	lines := strings.Split(`
+#hostname:port:database:username:password
+*:*:*:c42:lKj*$hL;(~
+*:*:*:c42ro:himom`, "\n")
+	fileutil.WriteLinesArray(lines, testFileName)
+}
+
+func Test_FindPgPassword(t *testing.T) {
+	if len(PgPassword("c42")) == 10 {
+		t.Log("find password test passed.")
+	} else {
+		t.Error("Correct password not found")
+	}
+}
