02a42fe0b22da547412a0bbc8feb2e6891ad96d5
Jon Carlson <jon@code42.com>
2014-09-14 22:24:37 -0500

    Changed the environment variables to match psql defaults: PGDATABASE, PGHOST, PGPORT, PGUSER

--- pgutil.go
+++ pgutil.go
@@ -1,19 +1,23 @@
 package pgutil
 
-import "database/sql"
-import _ "github.com/lib/pq"
 import "os"
 import "flag"
+import "log"
+import "time"
 import "fmt"
 import "strconv"
 import "strings"
+import "database/sql"
 import "github.com/joncrlsn/misc"
 import "github.com/joncrlsn/fileutil"
+import _ "github.com/lib/pq" // pg this import is for the PostgreSQL driver
 
 var p = fmt.Println
-var pgPassFile string = ".pgpass"
+var pgPassFile = ".pgpass"
 
-// Database connection info
+const isoFormat = "2006-01-02T15:04:05.000-0700"
+
+// DbInfo contains database connection info
 type DbInfo struct {
 	DbName    string
 	DbHost    string
@@ -23,25 +27,26 @@ type DbInfo struct {
 	DbOptions string
 }
 
-/*
- * Populates the database connection info from environment variables or runtime flags.
- * This calls flag.Parse(), so define any other program flags before calling this.
- */
+// Populate populates the database connection info from environment variables or runtime flags.
+// This calls flag.Parse(), so define any other program flags before calling this.
 func (dbInfo *DbInfo) Populate() {
-	userDefault := os.Getenv("DBUSER")
-	hostDefault := misc.CoalesceStrings(os.Getenv("DBHOST"), "localhost")
-	portDefaultStr := misc.CoalesceStrings(os.Getenv("DBPORT"), "5432")
-	passDefault := os.Getenv("PGPASS")
+	hostDefault := misc.CoalesceStrings(os.Getenv("PGHOST"), "localhost")
+	portDefaultStr := misc.CoalesceStrings(os.Getenv("PGPORT"), "5432")
+	dbDefault := os.Getenv("PGDATABASE")
+	userDefault := os.Getenv("PGUSER")
+	passDefault := os.Getenv("PGPASSWORD")
+	optionsDefault := os.Getenv("PGOPTIONS")
 
 	// port is a little different because it's an int
 	portDefault, _ := strconv.Atoi(portDefaultStr)
 	fmt.Println("portDefault", portDefault)
 
 	var dbUser = flag.String("U", userDefault, "db user")
-	var dbPass = flag.String("pw", "", "db password")
+	var dbPass = flag.String("pw", passDefault, "db password")
 	var dbHost = flag.String("h", hostDefault, "db host")
 	var dbPort = flag.Int("p", portDefault, "db port")
-	var dbName = flag.String("d", "", "db name")
+	var dbName = flag.String("d", dbDefault, "db name")
+	var dbOptions = flag.String("o", optionsDefault, "db options (eg. sslmode=disable)")
 
 	// This will parse all the flags defined for the program.  Not sure how to get around this.
 	flag.Parse()
@@ -54,13 +59,15 @@ func (dbInfo *DbInfo) Populate() {
 	}
 	// the password is a little different because it can also be found in ~/.pgpass
 	if len(dbInfo.DbPass) == 0 {
-		if len(passDefault) > 1 {
-			dbInfo.DbPass = passDefault
+		if dbPass == "prompt" {
+			dbInfo.DbPass = misc.PromptPassword("Enter password: ")
+		} else if len(dbPass) > 1 {
+			dbInfo.DbPass = dbPass
 		} else {
 			dbInfo.DbPass = PgPassword(dbInfo.DbUser)
-            if len(dbInfo.DbPass) == 0 {
-                dbInfo.DbPass = misc.PromptPassword("Enter password: ")
-            }
+			if len(dbInfo.DbPass) == 0 {
+				dbInfo.DbPass = misc.PromptPassword("Enter password: ")
+			}
 		}
 	}
 	if len(*dbHost) > 0 {
@@ -72,8 +79,12 @@ func (dbInfo *DbInfo) Populate() {
 	if len(*dbName) > 0 {
 		dbInfo.DbName = *dbName
 	}
+	if len(*dbOptions) > 0 {
+		dbInfo.DbOptions = *dbOptions
+	}
 }
 
+// ConnectionString returns the string needed by the postgres driver library to connect
 func (dbInfo *DbInfo) ConnectionString() string {
 	connString := "user=" + dbInfo.DbUser + " host=" + dbInfo.DbHost + " dbname=" + dbInfo.DbName + " password=" + dbInfo.DbPass
 	if len(dbInfo.DbOptions) > 0 {
@@ -82,28 +93,22 @@ func (dbInfo *DbInfo) ConnectionString() string {
 	return connString
 }
 
-/*
- * Opens a postgreSQL database connection using the DbInfo instance
- */
+// Open opens a postgreSQL database connection using the DbInfo instance
 func (dbInfo *DbInfo) Open() (*sql.DB, error) {
 	conn := dbInfo.ConnectionString()
 	db, err := sql.Open("postgres", conn)
 	return db, err
 }
 
-/*
- * Provides a model for adding to your own database executable
- */
+// DbUsage provides a model for adding to your own database executable
 func DbUsage() {
-	fmt.Fprintf(os.Stderr, "usage: %s [-h <host>] [-p <port>] [-d <dbname>] [-U <user>] [-pw <password>] \n", os.Args[0])
+	fmt.Fprintf(os.Stderr, "usage: %s [-h <host>] [-p <port>] [-d <dbname>] [-U <user>] [-pw <password>] [-o <db option>] \n", os.Args[0])
 	flag.PrintDefaults()
 	os.Exit(2)
 }
 
-/*
- * Parses the ~/.pgpass file and gets the password for the given user.  The current implementation
- * ignores the location field.
- */
+// PgPassword parses the ~/.pgpass file and gets the password for the given user.  The current implementation
+// ignores the location field.
 func PgPassword(user string) string {
 	pgPassPath := os.Getenv("HOME") + "/" + pgPassFile
 	exists, err := fileutil.Exists(pgPassPath)
@@ -122,9 +127,135 @@ func PgPassword(user string) string {
 		if strings.Contains(line, ":"+user+":") {
 			fields := strings.Split(line, ":")
 			password := fields[4]
-            fmt.Println("Used password from ~/.pgpass")
+			fmt.Println("Used password from ~/.pgpass")
 			return password
 		}
 	}
 	return ""
+}
+
+// QueryStrings returns row maps (keyed by the column name) in a channel.
+// Dynamically converts each column value to a SQL string value.
+// See http://stackoverflow.com/questions/23507531/is-golangs-sql-package-incapable-of-ad-hoc-exploratory-queries
+func QueryStrings(db *sql.DB, query string) (chan map[string]string, []string) {
+	rowChan := make(chan map[string]string)
+
+	rows, err := db.Query(query)
+	check("running query", err)
+	columnNames, err := rows.Columns()
+	check("getting column names", err)
+
+	go func() {
+
+		defer rows.Close()
+
+		vals := make([]interface{}, len(columnNames))
+		valPointers := make([]interface{}, len(columnNames))
+		// Copy
+		for i := 0; i < len(columnNames); i++ {
+			valPointers[i] = &vals[i]
+		}
+
+		for rows.Next() {
+			err = rows.Scan(valPointers...)
+			check("scanning a row", err)
+
+			row := make(map[string]string)
+			// Convert each cell to a SQL-valid string representation
+			for i, valPtr := range vals {
+				//fmt.Println(reflect.TypeOf(valPtr))
+				switch valueType := valPtr.(type) {
+				case nil:
+					row[columnNames[i]] = "null"
+				case []uint8:
+					row[columnNames[i]] = string(valPtr.([]byte))
+				case string:
+					row[columnNames[i]] = valPtr.(string)
+				case int64:
+					row[columnNames[i]] = fmt.Sprintf("%d", valPtr)
+				case float64:
+					row[columnNames[i]] = fmt.Sprintf("%f", valPtr)
+				case bool:
+					row[columnNames[i]] = fmt.Sprintf("%t", valPtr)
+				case time.Time:
+					row[columnNames[i]] = valPtr.(time.Time).Format(isoFormat)
+				case fmt.Stringer:
+					row[columnNames[i]] = fmt.Sprintf("%v", valPtr)
+				default:
+					row[columnNames[i]] = fmt.Sprintf("%v", valPtr)
+					fmt.Println("Warning, column %s is an unhandled type: %v", columnNames[i], valueType)
+				}
+			}
+			rowChan <- row
+		}
+		close(rowChan)
+	}()
+	return rowChan, columnNames
+}
+
+/*
+ * Returns row maps (keyed by the column name) in a channel.
+ * Dynamically converts each column value to a SQL string value.
+ * See http://stackoverflow.com/questions/23507531/is-golangs-sql-package-incapable-of-ad-hoc-exploratory-queries
+ */
+//func QuerySalValues(db *sql.DB, query string) (chan map[string]string, []string) {
+//	rowChan := make(chan map[string]string)
+//
+//	rows, err := db.Query(query)
+//	check("running query", err)
+//	columnNames, err := rows.Columns()
+//	check("getting column names", err)
+//
+//	go func() {
+//
+//		defer rows.Close()
+//
+//		vals := make([]interface{}, len(columnNames))
+//		valPointers := make([]interface{}, len(columnNames))
+//		// Copy
+//		for i := 0; i < len(columnNames); i++ {
+//			valPointers[i] = &vals[i]
+//		}
+//
+//		for rows.Next() {
+//			err = rows.Scan(valPointers...)
+//			check("scanning a row", err)
+//
+//			row := make(map[string]string)
+//			// Convert each cell to a SQL-valid string representation
+//			for i, valPtr := range vals {
+//				//fmt.Println(reflect.TypeOf(valPtr))
+//				switch valueType := valPtr.(type) {
+//				case nil:
+//					row[columnNames[i]] = "null"
+//				case []uint8:
+//					row[columnNames[i]] = "'" + string(valPtr.([]byte)) + "'"
+//				case string:
+//					row[columnNames[i]] = "'" + valPtr.(string) + "'"
+//				case int64:
+//					row[columnNames[i]] = fmt.Sprintf("%d", valPtr)
+//				case float64:
+//					row[columnNames[i]] = fmt.Sprintf("%f", valPtr)
+//				case bool:
+//					row[columnNames[i]] = fmt.Sprintf("%t", valPtr)
+//				case time.Time:
+//					row[columnNames[i]] = "'" + valPtr.(time.Time).Format(isoFormat) + "'"
+//				case fmt.Stringer:
+//					row[columnNames[i]] = fmt.Sprintf("%v", valPtr)
+//				default:
+//					row[columnNames[i]] = fmt.Sprintf("%v", valPtr)
+//					fmt.Println("Column %s is an unhandled type: %v", columnNames[i], valueType)
+//				}
+//			}
+//			rowChan <- row
+//		}
+//		close(rowChan)
+//	}()
+//	return rowChan, columnNames
+//}
+
+func check(msg string, err error) {
+	if err != nil {
+		log.Fatal("Error "+msg, err)
+	}
 }
